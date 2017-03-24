55de3aa16acb43d884777b4c9c2761235163271b
akramkammoun <akram.kamoun@gmail.com>
2017-03-24 23:33:13 +0100

    Adding projects

--- SCGenerator/src/main/java/large3/soap/package-info.java
+++
@@ -1,6 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:large3:large3:1.0")
-package large3.soap;
-
-import large3.output.*;
-import large3.input.*;
-
--- SCGenerator/src/main/java/the1/soap/package-info.java
+++
@@ -1,6 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:the1:the1:1.0")
-package the1.soap;
-
-import the1.output.*;
-import the1.input.*;
-
